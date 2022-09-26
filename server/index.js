a9134a00010f77d4cddfb149e387bae6d24d73ff
arthurbz <arthurbziero@gmail.com>
2022-09-25 22:54:36 -0300

    refinements :p

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 const express = require('express')
-const consign = require('consign');
+const consign = require('consign')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const PORT = 5000 
 
 const app = express()
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.json())
 
-consign().include('controllers').into(app);
+consign().include('controllers').into(app)
 
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+    console.log(`Running on port ${PORT}`)
 })
